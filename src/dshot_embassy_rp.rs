4bc514b196ef880da936318fef664e0d8810fbde
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2025-06-07 17:59:54 -0400

    Fix Incorrectly Maxing Throttle

--- src/dshot_embassy_rp.rs
+++ src/dshot_embassy_rp.rs
@@ -198,7 +198,7 @@ impl <'d,PIO : Instance> super::DshotPioTrait<1> for DshotPio<'d,1,PIO> {
     
     /// Send any valid DShot value to the ESC.
     fn command(&mut self, command: [u16; 1]) {
-        self.pio_instance.sm0.tx().push(command[0].min(dshot::THROTTLE_MAX) as u32);
+        self.pio_instance.sm0.tx().push(command[0] as u32);
     }
     
     /// Set the direction of rotation for each motor
@@ -221,8 +221,8 @@ impl <'d,PIO : Instance> super::DshotPioTrait<2> for DshotPio<'d,2,PIO> {
     
     /// Send any valid DShot value to the ESC.
     fn command(&mut self, command: [u16; 2]) {
-        self.pio_instance.sm0.tx().push(command[0].min(dshot::THROTTLE_MAX) as u32);
-        self.pio_instance.sm0.tx().push(command[1].min(dshot::THROTTLE_MAX) as u32);
+        self.pio_instance.sm0.tx().push(command[0] as u32);
+        self.pio_instance.sm0.tx().push(command[1] as u32);
     }
     
     /// Set the direction of rotation for each motor
@@ -248,9 +248,9 @@ impl <'d,PIO : Instance> super::DshotPioTrait<3> for DshotPio<'d,3,PIO> {
     
     /// Send any valid DShot value to the ESC.
     fn command(&mut self, command: [u16; 3]) {
-        self.pio_instance.sm0.tx().push(command[0].min(dshot::THROTTLE_MAX) as u32);
-        self.pio_instance.sm0.tx().push(command[1].min(dshot::THROTTLE_MAX) as u32);
-        self.pio_instance.sm0.tx().push(command[2].min(dshot::THROTTLE_MAX) as u32);
+        self.pio_instance.sm0.tx().push(command[0] as u32);
+        self.pio_instance.sm0.tx().push(command[1] as u32);
+        self.pio_instance.sm0.tx().push(command[2] as u32);
     }
     
     /// Set the direction of rotation for each motor
@@ -279,10 +279,10 @@ impl <'d,PIO : Instance> super::DshotPioTrait<4 > for DshotPio<'d,4,PIO> {
     
     /// Send any valid DShot value to the ESC.
     fn command(&mut self, command: [u16; 4]) {
-        self.pio_instance.sm0.tx().push(command[0].min(dshot::THROTTLE_MAX) as u32);
-        self.pio_instance.sm0.tx().push(command[1].min(dshot::THROTTLE_MAX) as u32);
-        self.pio_instance.sm0.tx().push(command[2].min(dshot::THROTTLE_MAX) as u32);
-        self.pio_instance.sm0.tx().push(command[3].min(dshot::THROTTLE_MAX) as u32);
+        self.pio_instance.sm0.tx().push(command[0] as u32);
+        self.pio_instance.sm0.tx().push(command[1] as u32);
+        self.pio_instance.sm0.tx().push(command[2] as u32);
+        self.pio_instance.sm0.tx().push(command[3] as u32);
     }
     
     /// Set the direction of rotation for each motor
